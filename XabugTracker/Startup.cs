25777274cd1e46ee55f1625d8b207c8ae6a4d0bf
brandonvondo <68125476+brandonvondo@users.noreply.github.com>
2020-08-03 14:41:23 -0500

    Initial commit
    Git ignore updated to exclude sensitive files

---
+++ XabugTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XabugTracker.Startup))]
+namespace XabugTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
